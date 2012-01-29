dff43f8b5921e8cbc550545fbbdab08bf08f3ead
PhillipKregg <phillipkregg@gmail.com>
2012-01-29 15:52:35 -0600

    Added a dropdown form on the Index page of the Restaurant model.  It is being populated by the ViewBag object in the RestaurantController.

--- OdeToFood/Controllers/RestaurantController.cs
+++ OdeToFood/Controllers/RestaurantController.cs
@@ -17,14 +17,13 @@ namespace OdeToFood.Controllers
 
         public ActionResult Index(string state)
         {
+
+            ViewBag.States = _db.Restaurants.Select(r => r.Address.State).Distinct();
+
             var model = _db.Restaurants
                             .OrderByDescending(r => r.Address.City)
                             .Where(r => r.Address.State == state || (state == null));
-                            
-                       
-            
-            
-            
+                      
             //var model = from r in _db.Restaurants
             //            orderby r.Address.City descending
             //            where r.Address.State == state || state == null
