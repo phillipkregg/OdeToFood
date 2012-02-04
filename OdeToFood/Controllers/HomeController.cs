c2760d19c417dc8bb4ad5d70e38ad22646750302
PhillipKregg <phillipkregg@gmail.com>
2012-02-03 18:31:31 -0600

    Couldn't push behind proxy server - making sure everything is up to date

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Queries;
+using System.Threading;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+
+        OdeToFoodDB _db = new OdeToFoodDB();
+
+        public PartialViewResult LatestReview()
+        {
+            Thread.Sleep(2000);
+            var review = _db.Reviews.FindTheLatest(1).Single();
+            return PartialView("_Review", review);
+
+
+        }
+
         public ViewResult Index()
         {
             ViewBag.Message = "Welcome to Ode To Food";
